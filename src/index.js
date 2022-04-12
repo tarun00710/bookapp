b2eeac2ea1ab1bfb5088f1c04911cec645031146
tarun00710 <tarunlal07@gmail.com>
2022-04-12 17:15:31 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import BookCon from "./Context/BookCon";
+import BookCon from '../src/Context/BookCon'
 
 ReactDOM.render(
   <React.StrictMode>
